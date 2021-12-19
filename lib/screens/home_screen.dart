5f3f759d18903e396589a3a25940235cb8225a73
Phonekham <phone.madridista@gmail.com>
2021-12-19 14:49:13 +0700

    order screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_food_delivery/data/data.dart';
 import 'package:flutter_food_delivery/models/restaurant.dart';
+import 'package:flutter_food_delivery/screens/cart_screen.dart';
 import 'package:flutter_food_delivery/screens/restaurant_screen.dart';
 import 'package:flutter_food_delivery/widgets/rating_stars.dart';
 import 'package:flutter_food_delivery/widgets/recent_orders.dart';
@@ -105,7 +106,8 @@ class _HomeScreenState extends State<HomeScreen> {
           ElevatedButton(
               style: ElevatedButton.styleFrom(
                   textStyle: TextStyle(fontSize: 20.0, color: Colors.white)),
-              onPressed: () => {},
+              onPressed: () => Navigator.push(
+                  context, MaterialPageRoute(builder: (_) => CartScreen())),
               child: Text('Cart (${currentUser.cart.length})'))
         ],
       ),
